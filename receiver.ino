fc5b23eec0bc6f6e5b3f12d1f446081a5d411f66
RH11180 <34293035+RH11180@users.noreply.github.com>
2018-03-16 00:24:07 -0400

    Add files via upload

---
+++ receiver.ino
@@ -0,0 +1,53 @@
+int LED=3;
+int started = 0;
+int ended = 0;
+char incoming ;
+char data[4];
+byte digi;
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(LED, OUTPUT);
+
+
+}
+
+void loop() {
+while (Serial.available()>0)
+{
+  incoming = Serial.read();
+  if (incoming == '<')
+  {
+    started = 1;
+    digi=0;
+    data[digi] = '\0';
+  }
+  else if (incoming == '>')
+  {
+    ended = 1;
+    break;
+   
+  }
+else
+{
+  if (digi<5)
+  {
+    data[digi] = incoming;
+    digi++;
+    data[digi] = '\0';
+  }
+}
+}
+if (started==1 && ended==1)
+{
+  int value = atoi(data);
+
+  analogWrite(LED, value);
+  digi=0;
+  data[digi] = '\0';
+  started=0;
+  ended =0;
+  
+}
+}
