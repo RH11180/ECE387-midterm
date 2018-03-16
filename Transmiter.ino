234a25042fc3d079ba9d2b4f1cac875b34d804d8
RH11180 <34293035+RH11180@users.noreply.github.com>
2018-03-15 20:38:56 -0400

    Add files via upload

---
+++ Transmiter.ino
@@ -0,0 +1,51 @@
+#define trig 2 
+#define echo 3 
+int cm; //distance 
+float temp; //   
+const int aRead = A0; 
+int value, threshold; 
+
+void setup() {
+  
+	Serial.begin(9600); 
+  pinMode(trig, OUTPUT);  
+  pinMode(echo, INPUT);  
+}
+
+void loop() {
+  //trigger the distance mesurement 
+  digitalWrite(trig, LOW); // send low to trig
+  delayMicroseconds(2);    //wait more than 10us aka 2 ms
+  digitalWrite(trig,HIGH); //send high to trig 
+  delayMicroseconds(10);    //wait 10 ms
+  digitalWrite(trig, LOW); //send low and end trigger
+    
+  temp = float(pulseIn(echo, HIGH)); //save the time of echo
+  //pulse return time from high to low
+
+
+  /* Math part:(
+   *  sound speed 340m/s 340000cm/1000000 us
+   *  round trip divide by 2
+   *  temp*((34/1000))/2
+   */
+//  Serial.print(cm);//  
+   cm = (temp * 17 )/1000; //time to distance
+  	value = cm;
+  	//Map the analog value to pwm value
+  	value = map (value, 0, 90, 0, 511);
+    threshold=200-value;
+    if (threshold>40)
+    {value=200;}
+    else
+    {value=0; }
+    
+	//Send the message:
+	//Serial.print(vav);
+	Serial.print('<'); 	//Start
+ 	Serial.print(value);
+	Serial.println('>');//End
+}
+
+
+  
---
+++ receiver.ino
@@ -0,0 +1,54 @@
+const int LED=3;
+
+bool started = false;
+bool ended = false;
+char incoming ;
+char data[4];
+byte digi;
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(LED, OUTPUT);
+
+
+}
+
+void loop() {
+while (Serial.available()>0)
+{
+  incoming = Serial.read();
+  if (incoming == '<')
+  {
+    started = true;
+    digi=0;
+    data[digi] = '\0';
+  }
+  else if (incoming == '>')
+  {
+    ended = true;
+    break;
+   
+  }
+else
+{
+  if (digi<5)
+  {
+    data[digi] = incoming;
+    digi++;
+    data[digi] = '\0';
+  }
+}
+}
+if (started && ended)
+{
+  int value = atoi(data);
+
+  analogWrite(LED, value);
+  digi=0;
+  data[digi] = '\0';
+  started=false;
+  ended =false;
+  
+}
+}
